8ab651acc92a24db188c97e6dfa84edc9c47b784
Akhil Vase <avase33@gmail.com>
2026-07-09 08:52:38 -0500

    feat(utils): add package and env var diff with formatted output (20260709085153)

--- src/utils/diff.ts
+++ src/utils/diff.ts
@@ -1,4 +1,4 @@
-// Environment diff utilities -- 2026-07-09 08:50:48
+// Environment diff utilities -- 2026-07-09 08:51:53
 import { EnvSnapshot } from './snapshot';
 
 export interface DiffResult {
