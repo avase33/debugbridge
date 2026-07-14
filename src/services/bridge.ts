9541700c893bec01282616fa1a183a98a1954349
Akhil Vase <avase33@gmail.com>
2026-07-14 15:07:31 -0500

    feat(services): add BridgeService with snapshot capture and comparison (20260714150657)

--- src/services/bridge.ts
+++ src/services/bridge.ts
@@ -1,4 +1,4 @@
-// DebugBridge core service -- 2026-07-14 15:05:56
+// DebugBridge core service -- 2026-07-14 15:06:57
 import { captureSnapshot, EnvSnapshot } from '../utils/snapshot';
 import { diffPackages, diffEnvVars, formatDiff } from '../utils/diff';
 import * as fs from 'fs';
