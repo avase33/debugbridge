d1aaba2bad1c9dc7334c0d6706f313ef290574f9
Akhil Vase <avase33@gmail.com>
2026-07-17 22:10:11 -0500

    feat(utils): add environment snapshot capture with git info (20260717220946)

--- src/utils/snapshot.ts
+++ src/utils/snapshot.ts
@@ -1,4 +1,4 @@
-// Environment snapshot utilities -- 2026-07-17 22:07:31
+// Environment snapshot utilities -- 2026-07-17 22:09:46
 import { execSync } from 'child_process';
 import * as os from 'os';
 
