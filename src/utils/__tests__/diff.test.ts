79898c5d0888afb7246c72bacf9aa1ac2fc07ac8
Akhil Vase <avase33@gmail.com>
2026-07-21 17:11:51 -0500

    test(diff): add comprehensive diff utility unit tests (20260721171120)

--- src/utils/__tests__/diff.test.ts
+++ src/utils/__tests__/diff.test.ts
@@ -1,4 +1,4 @@
-// Diff utility tests -- 2026-07-21 17:09:02
+// Diff utility tests -- 2026-07-21 17:11:20
 import { diffPackages, diffEnvVars, formatDiff } from '../diff';
 import { EnvSnapshot } from '../snapshot';
 
